84b2cd3f078d2b496e0ef44e9f24a0b7012c25f5
vladyslavtsurkan <vtsurkan2002@gmail.com>
2023-08-07 22:37:00 +0300

    Added tailwindcss into project

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,3 @@
 <template>
-  <div class="wrapper">
-    <HelloWorld msg="You did it!" />
-    <HelloWorld msg="AHAHAHAHHA"></HelloWorld>
-  </div>
+  <h1 class="mt-8 text-center text-6xl">Hello Vue</h1>
 </template>
-
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
