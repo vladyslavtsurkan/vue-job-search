657d5f19cb9c764686a8dc683b4c0317f90ae39e
vladyslavtsurkan <vtsurkan2002@gmail.com>
2023-08-11 23:36:19 +0300

    Update libraries, added test for MainNav component

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -3,9 +3,6 @@
     <div class="fixed left-0 top-0 h-16 w-full bg-white">
       <div class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8">
         <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
-        <h2 class="ml-8 flex h-full items-center">
-          Developed by {{ author.firstName }} {{ author.lastName }}
-        </h2>
       </div>
     </div>
   </header>
@@ -17,10 +14,6 @@ export default {
   data() {
     return {
       company: "Bobo Careers",
-      author: {
-        firstName: "Vladyslav",
-        lastName: "Tsurkan",
-      },
     };
   },
 };
---
+++ tests/unit/components/MainNav.test.js
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/vue";
+
+import MainNav from "@/components/MainNav.vue";
+
+describe("MainNav", () => {
+  it("displays company name", () => {
+    render(MainNav);
+    const companyName = screen.getByText("Bobo Careers");
+    expect(companyName).toBeInTheDocument();
+  });
+});
