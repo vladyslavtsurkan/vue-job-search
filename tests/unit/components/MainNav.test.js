a4ab2ab7b7f1135e673bc45e926960a398fd11d4
vladyslavtsurkan <vtsurkan2002@gmail.com>
2023-08-12 15:01:01 +0300

    Add nav tag with items to MainNav component and write the test

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -8,4 +8,18 @@ describe("MainNav", () => {
     const companyName = screen.getByText("Bobo Careers");
     expect(companyName).toBeInTheDocument();
   });
+
+  it("displays menu items for navigation", () => {
+    render(MainNav);
+    const navigationMenuItems = screen.getAllByRole("listitem");
+    const navigationMenuTexts = navigationMenuItems.map((item) => item.textContent);
+    expect(navigationMenuTexts).toEqual([
+      "Teams",
+      "Locations",
+      "Life at Bobo Corp",
+      "How we hire",
+      "Students",
+      "Jobs",
+    ]);
+  });
 });
