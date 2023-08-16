a7e2f7e73766d7bba2c7e45e3b0bee525be7e900
vladyslavtsurkan <vtsurkan2002@gmail.com>
2023-08-16 22:19:52 +0300

    Added JobSearchForm component and refactoring other components

---
+++ src/components/JobSearch/TheHero.vue
@@ -0,0 +1,36 @@
+<template>
+  <main>
+    <section class="flex h-screen flex-col pb-20 pt-10">
+      <div class="grid grid-cols-12">
+        <div class="col-span-1 col-start-1"></div>
+
+        <div class="col-span-5 col-start-2">
+          <the-headline />
+          <job-search-form />
+        </div>
+
+        <div class="col-span-5 col-start-7 self-center justify-self-center">
+          <img
+            class="h-80 w-80 object-contain"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/2367px-Vue.js_Logo_2.svg.png"
+          />
+        </div>
+
+        <div class="col-span-1 col-start-12"></div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+import TheHeadline from "@/components/JobSearch/TheHeadline.vue";
+import JobSearchForm from "@/components/JobSearch/JobSearchForm.vue";
+
+export default {
+  name: "TheHero",
+  components: {
+    TheHeadline,
+    JobSearchForm,
+  },
+};
+</script>
